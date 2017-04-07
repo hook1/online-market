442c80760a5fd326c2dbed46cdc15258d82e3408
TarasSluka <sluka.taras@gmail.com>
2017-04-07 20:28:19 +0300

    initial commit

---
+++ src/main/java/com/sluka/taras/OnlineMarketApplication.java
@@ -0,0 +1,12 @@
+package com.sluka.taras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineMarketApplication.class, args);
+    }
+}
